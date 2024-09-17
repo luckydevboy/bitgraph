01466d33b3094b2289a131537f6ebb28b5d9fe07
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-09-17 20:24:29 +0330

    chore: handle layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="grid grid-cols-12 gap-6 p-6">
+      <Card className="col-span-9">
+        <CardHeader></CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsum.
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+      <Card className="col-span-3">
+        <CardHeader></CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsum.
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+      <Card className="col-span-12">
+        <CardHeader></CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsum.
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </div>
+  );
 }
